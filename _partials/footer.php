03f2fb4eb6982b492c8a81686723f0debbfa701f
ecuanaso <miestilo23@gmail.com>
2014-11-10 14:42:12 -0500

    Adding pages

---
+++ _partials/footer.php
@@ -0,0 +1,16 @@
+
+
+		<footer>
+				<p>&copy <a href="www.daveswebapps.com">Daveswebapps.com</a></p>
+			</footer>
+
+</div>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script src="http://cloud.github.com/downloads/wycats/handlebars.js/handlebars-1.0.0.beta.6.js"></script>
+<script src="js/scripts.js"></script>
+
+
+</body>
+</html>
